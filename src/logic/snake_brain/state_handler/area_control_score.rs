e733b2b40417b35b6f8f07a1fd3c9c119330730f
MaxHissen <125509779+MaxHissen@users.noreply.github.com>
2023-12-12 00:51:15 -0800

    Update area_control_score.rs

--- src/logic/snake_brain/state_handler/area_control_score.rs
+++ src/logic/snake_brain/state_handler/area_control_score.rs
@@ -3,7 +3,13 @@ use crate::logic::snake_brain::SIZE;
 
 use std::collections::VecDeque;
 
-pub fn get_area_control_score(state : &State, player : &u8) -> (u8, [Vec<u8>; 2]) {
+pub fn get_area_control_score(state : &State, player : &u8, is_body : bool) -> (u8, [Vec<u8>; 2], bool) {
+    //is body means that the area control starts from every body part
+
+    let mut me_tail_spot : (u8, u8) = (11, 11);
+    let mut enemy_tail_spot : (u8, u8) = (11, 11);
+    let mut can_reach_me_tail = false;
+    get_tail_positions(state, &mut me_tail_spot, &mut enemy_tail_spot, player);
 
 
     let mut area_i_control = 0;
@@ -23,21 +29,29 @@ pub fn get_area_control_score(state : &State, player : &u8) -> (u8, [Vec<u8>; 2]
         }
     }
 
+    if is_body{
+        for y in 0..SIZE{
+            for x in 0..SIZE{
+                let cell = state.board_struct[y][x];
+                if cell != 0b11111111 && cell != 0{
+                    let index = (cell&0b10000000) >> 7;
+                    frontier.push_back((x as u8, y as u8, index, 0));
+                }
+            }
+        }
+    }
+
     let mut iteration = 0;
 
     let mut gone_to : [[u8; SIZE]; SIZE] = [[4; SIZE]; SIZE];
-    //print_board_struct(gone_to);
-    
-    //println!("{:?}", frontier);
+
     while let Some(cur) = frontier.pop_front() {
-        //println!();
         iteration += 1;
 
         let cur_x = cur.0;
         let cur_y = cur.1;
         let cur_index = cur.2;
         let cur_distance = cur.3;
-        //println!("index: {:?} {:?} {:?}", cur_index, cur_x, cur_y);
 
         if state.board_struct[cur_y as usize][cur_x as usize]&0b01111111 > cur_distance && state.board_struct[cur_y as usize][cur_x as usize] != 0b11111111{
             continue;
@@ -46,6 +60,12 @@ pub fn get_area_control_score(state : &State, player : &u8) -> (u8, [Vec<u8>; 2]
             continue;
         }
         //println!("index: {:?}", cur_index);
+
+        if (cur_x, cur_y) == me_tail_spot{
+            if cur_index == *player{
+                can_reach_me_tail = true;
+            }
+        }
 
         if state.board_struct[cur_y as usize][cur_x as usize] == 0b11111111{
             food_distances[cur_index as usize].push(cur_distance);
@@ -70,23 +90,37 @@ pub fn get_area_control_score(state : &State, player : &u8) -> (u8, [Vec<u8>; 2]
         }
     }
 
-    return (area_i_control, food_distances);
+    return (area_i_control, food_distances, can_reach_me_tail);
 }
 
-pub fn body_in_center_score(state : &State) -> u8{
-    let mut in_center_score : u16 = 0;
+
+pub fn get_tail_positions(state : &State, me_tail_spot : &mut (u8, u8), enemy_tail_spot : &mut (u8, u8), player : &u8){
+    let mut smallest_me_index = (SIZE*SIZE) as u8;
+    let mut smallest_enemy_index = (SIZE*SIZE) as u8;
 
     for y in 0..SIZE{
         for x in 0..SIZE{
             let cell = state.board_struct[y][x];
-
-            let near_middle_x = -(x as i16 - SIZE as i16/2).abs() + SIZE as i16/2;
-            let near_middle_y = -(y as i16 - SIZE as i16/2).abs() + SIZE as i16/2;
-            if (cell&0b11000000) >> 6 == 0 && cell > 0{
-                in_center_score += ((cell/10) as i16 * (near_middle_x + near_middle_y)) as u16;
+            if cell != 0b11111111{
+                let index = (cell & 0b10000000) >> 7;
+                let size = cell&0b01111111;
+                if size >= 1{
+                    if index == *player{
+                        if size < smallest_me_index{
+                            smallest_me_index = size;
+                            me_tail_spot.0 = x as u8;
+                            me_tail_spot.1 = y as u8;
+                        }
+                    }
+                    if index == (1 - *player){
+                        if size < smallest_enemy_index{
+                            smallest_enemy_index = size;
+                            enemy_tail_spot.0 = x as u8;
+                            enemy_tail_spot.1 = y as u8;
+                        }
+                    }
+                }
             }
         }
     }
-
-    return in_center_score as u8;
 }
